7f4ea8b171997287438b2b49004aedd30b47bbe4
Matwey <linnov1703@gmail.com>
2023-02-18 22:01:29 +0300

    задание к 6 семинару

---
+++ task1/Program.cs
@@ -0,0 +1,41 @@
+﻿int m = InputNum("Введите длину массива ");
+
+int[] array = CreateArray(m);
+PrintArray(array, "Базовый массив");
+
+int num = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        num++;
+    }
+}
+
+System.Console.WriteLine($"Количество значений больше 0 = {num}");
+
+int[] CreateArray(int m)
+{
+    int[] array = new int[m];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = InputNum("Введите числа через Enter ");                                         
+    }
+    return array;
+}
+
+void PrintArray(int[] array, string msg)
+{
+    System.Console.WriteLine(msg);
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int InputNum(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
---
+++ task2/Program.cs
@@ -0,0 +1,37 @@
+﻿// y = k1 * x + b1, y = k2 * x + b2
+// k1*x+b1=k2*x+b2 
+// k1*x-k2x=b2-b1
+// x(k1-k2)= b2-b1
+// x=(b2-b1)/(k1-k2)
+
+int IputNum(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(System.Console.ReadLine());
+}
+
+double b1 = IputNum("Введите значение b1: ");
+double k1 = IputNum("Введите число k1: ");
+double b2 = IputNum("Введите значение b2: ");
+double k2 = IputNum("Введите число k2: ");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k2 * x + b2;
+
+if (k1 == k2 && b1 == b2)
+{
+    System.Console.WriteLine($"Прямые совпадают");
+}
+else if (k1 == k2 && b1 != b2)
+{
+    System.Console.WriteLine($"Прямые параллельны");
+}
+else
+{
+    System.Console.WriteLine($"Прямые пересекутся в точке с координатами x > {x} y > {y}");
+}
+
+
+
+
+
